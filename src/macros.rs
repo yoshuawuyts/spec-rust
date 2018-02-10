66eb11014297770e8df8bb20aa34aba0b7791901
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-11 00:01:49 +0100

    arm matching macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,9 +1,17 @@
-// /// ```rust
-// /// #[macro_use] extern crate spec;
-// /// spec::macros::foo!(y => 3);
-// /// ```
-
-// macro_rules! foo {
-//   (x => $e:expr) => (println!("mode x {}", $e));
-//   (y => $e:expr) => (println!("mode x {}", $e));
-// }
+/// Try out a macro
+///
+/// ```rust
+/// #[macro_use] extern crate spec;
+/// fn main() {
+///   let res = foo!(x => 3);
+///   assert_eq!(res, 3);
+///
+///   let res = foo!(y => 3);
+///   assert_eq!(res, 6);
+/// }
+/// ```
+#[macro_export]
+macro_rules! foo {
+  (x => $e:expr) => ($e);
+  (y => $e:expr) => (2 * $e);
+}

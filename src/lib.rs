0c4b06a0606f8f3bcdc11fa0f1180a60a7b35a2e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-03-14 16:55:56 +0100

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
+pub mod submodule;
+
 mod _unsafe;
 mod heap;
 mod macros;
-mod submodule;
 mod vectors;
 
 pub use _unsafe::*;
 pub use heap::*;
 pub use macros::*;
-pub use submodule::*;
 pub use vectors::*;
--- src/submodule/mod.rs
+++ src/submodule/mod.rs
@@ -1,5 +1,8 @@
+//! Example submodule with internal modules.
+
 mod internal;
 
+/// calls internal::print();
 pub fn print() {
   internal::print();
 }
